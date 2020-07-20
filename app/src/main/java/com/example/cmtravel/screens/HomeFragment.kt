6efbbecef9a7db0b60fcaec9dea99eeb4c07dd92
sjitprogrammer@gmail.com <sjitprogrammer@gmail.com>
2020-07-21 00:37:12 +0700

    update item to gridView 2 column and searchView

--- app/src/main/java/com/example/cmtravel/screens/HomeFragment.kt
+++ app/src/main/java/com/example/cmtravel/screens/HomeFragment.kt
@@ -5,25 +5,62 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.cmtravel.R
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 private const val TAG = "HomeFragment"
 
 class HomeFragment : Fragment() {
-    val tript: ArrayList<Trips> = ArrayList()
+    val trips: ArrayList<Trips> = ArrayList()
+    val tempItem: ArrayList<Trips> = ArrayList()
+    val db = FirebaseFirestore.getInstance()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val db = FirebaseFirestore.getInstance()
+        fetchData()
+        searchView_trip.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
 
-        var recyclerView = view.findViewById(R.id.recyclerView) as RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL ,false)
-        val adapter = TripsAdapter(tript, requireContext())
-        recyclerView.adapter = adapter
+            override fun onQueryTextChange(newText : String?): Boolean {
+                Log.d(TAG,newText)
+                if(newText!!.isNotEmpty()){
+                    tempItem.clear()
+                    val search = newText.toLowerCase(Locale.getDefault())
+
+                    trips.forEach{
+                        if(it.title?.toLowerCase(Locale.getDefault())?.contains(search)!!){
+                            tempItem.add(it)
+                        }
+                    }
+                    recyclerView_home.adapter?.notifyDataSetChanged()
+                }else{
+                    tempItem.clear()
+                    tempItem.addAll(trips)
+                    recyclerView_home.adapter?.notifyDataSetChanged()
+                }
+                return true
+            }
+
+        })
+    }
+
+    private fun fetchData() {
 
         db.collection("trips")
             .get()
@@ -32,7 +69,7 @@ class HomeFragment : Fragment() {
                     for (document in task.result!!) {
                         Log.d(TAG, document.id + " => " + document.data)
 //                        val myObject = document.toObject(Trips::class.java)
-                        tript.add(
+                        trips.add(
                             Trips(
                                 document.data["title"] as String,
                                 document.data["description"] as String,
@@ -42,23 +79,24 @@ class HomeFragment : Fragment() {
                             )
                         )
                     }
-
-                    adapter.notifyDataSetChanged()
-                    Log.e(TAG,""+adapter)
+                    showData(trips)
+                    tempItem.addAll(trips)
                 } else {
                     Log.w(TAG, "Error getting documents.", task.exception)
                 }
             }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-        return view
+    }
+
+    private fun showData(trips: List<Trips>) {
+
+        val gridLayoutManager = GridLayoutManager(requireContext(), 2)
+        recyclerView_home.layoutManager = gridLayoutManager
+        recyclerView_home.adapter = TripsAdapter(tempItem, requireContext())
     }
 
 
--- app/src/main/java/com/example/cmtravel/screens/TripsAdapter.kt
+++ app/src/main/java/com/example/cmtravel/screens/TripsAdapter.kt
@@ -1,52 +1,48 @@
 package com.example.cmtravel.screens
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cmtravel.R
-import com.squareup.picasso.Picasso
 
-class TripsAdapter(private var items: List<Trips>, private val context: Context) :
-    RecyclerView.Adapter<TripsAdapter.ViewHolder>() {
+class TripsAdapter (val items:List<Trips>,val context:Context) : RecyclerView.Adapter<TripsAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.trip_item,
-                parent,
-                false
-            )
-        )
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.trip_item,parent,false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount() = items.size
+    override fun getItemCount(): Int {
+        return items.size
+    }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-//        Glide
-//            .with(context)
-//            .load(item.imageUrl)
-//            .centerCrop()
-//            .placeholder(ColorDrawable(Color.BLACK))
-//            .into(holder.imageView);
-        Picasso.get().load(item.imageUrl).into(holder.imageView)
-        holder.title.text = item.title.toString()
-        holder.price.text = "฿" + item.price.toString()
-        Log.e("onBindViewHolder", "" + item.title.toString())
+//        Picasso.get().load(item.imageUrl).into(holder.imageView);
+        Glide.with(context)
+            .load(item.imageUrl)
+            .placeholder(R.drawable.placeholder_image)
+            .error(R.drawable.placeholder_image)
+            .into(holder.imageView);
+        holder.text_title.text = item.title
+        holder.text_price.text = "฿ "+item.price.toString()
+
+        holder.itemView.setOnClickListener {
+            Log.e("TripsAdapter",item.title)
+        }
+
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.icon_image_view)
-        val title: TextView = view.findViewById(R.id.title_text_view)
-        val price: TextView = view.findViewById(R.id.price_texView)
+    class ViewHolder(val view: View) :RecyclerView.ViewHolder(view){
+        val imageView:ImageView = view.findViewById(R.id.icon_image_view)
+        val text_title:TextView = view.findViewById(R.id.title_text_view)
+        val text_price:TextView = view.findViewById(R.id.price_texView)
     }
-
-
 }
+
